7934c5cdd77041b32be3a9f506d19fccd635f7c3
jejune86 <jejune86@gmail.com>
2024-11-22 00:15:36 +0900

    hyperparameter tuning

--- data_loader.py
+++ data_loader.py
@@ -10,7 +10,9 @@ VALIDATION_SIZE = 0.2
 DEFAULT_SIZE = 48
 
 # 클래스 레이블 정의
-classes = ["angry", "disgusted", "happy", "neutral", "sad", "surprised"]
+classes = ["angry", "fearful", "happy", "neutral", "sad", "surprised"]
+# disgusted는 데이터가 너무 부족하여, 사용 x
+
 NUM_CLASSES = len(classes)
 
 def load_train_data(
@@ -27,7 +29,7 @@ def load_train_data(
         train_path,
         labels="inferred",
         label_mode="int",
-        image_size=img_size,
+        image_size=(img_size, img_size),
         color_mode="grayscale" if gray else "RGB",
         batch_size=batch_size,
         shuffle=False,  # shuffle=False로 먼저 불러오고 stratified sampling 처리
@@ -45,45 +47,46 @@ def load_train_data(
     # 배치 처리된 데이터를 하나의 배열로 합침
     all_images = np.concatenate(all_images, axis=0)
     all_labels = np.concatenate(all_labels, axis=0)
-
-    # Stratified split: 데이터를 훈련 세트와 검증 세트로 나누기
-    X_train, X_val, y_train, y_val = train_test_split(
-        all_images, all_labels, test_size=VALIDATION_SIZE, stratify=all_labels, random_state=RANDOM_STATE
-    )
-
-    # TensorFlow Dataset으로 변환
-    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-    val_dataset = tf.data.Dataset.from_tensor_slices((X_val, y_val))
-
+    
     # 데이터 전처리 함수 정의
     def preprocess_image(image, label):
         if normalization:
             image = image / 255.0  # 정규화
         if flatten:
-            image = tf.reshape(image, [-1])  # 평탄화
+            image = image.flatten()  # 평탄화
         return image, label
 
-    def augment_image(image, label):
-        if label != classes.index("happy") :
-            image = tf.image.random_flip_left_right(image)
-            image = tf.image.random_brightness(image, max_delta=0.1)
-            image = tf.image.random_contrast(image, lower=0.8, upper=1.2)
-        return image, label
+    all_images, all_labels = preprocess_image(all_images, all_labels)
+    
+    # Stratified split: 데이터를 훈련 세트와 검증 세트로 나누기
+    X_train, X_val, y_train, y_val = train_test_split(
+        all_images, all_labels, test_size=VALIDATION_SIZE, stratify=all_labels, random_state=RANDOM_STATE
+    )
+
+
+    def augment_image(images):
+
+        images = np.array([np.fliplr(img) for img in images])
+        
+        brightness_factor = np.random.uniform(0.9, 1.1, size=images.shape[0])
+        images = np.array([np.clip(img * factor, 0, 255).astype(np.uint8) for img, factor in zip(images, brightness_factor)])
+        
+        contrast_factor = np.random.uniform(0.8, 1.2, size=images.shape[0])
+        images = np.array([np.clip(img * factor, 0, 255).astype(np.uint8) for img, factor in zip(images, contrast_factor)])
+
+        return images
 
     # 학습 데이터에는 증강 적용
-    train_dataset = train_dataset.map(lambda x, y: preprocess_image(x, y))
+    non_happy_indices = y_train != classes.index("happy")
+    X_train_non_happy = X_train[non_happy_indices]
+    y_train_non_happy = y_train[non_happy_indices]
     
-    non_happy_dataset = train_dataset.filter(lambda x, y: y != classes.index("happy"))
+    # Convert lists to np.array
+    augmented_images = augment_image(X_train_non_happy)
     
-    augmented_dataset = non_happy_dataset.map(lambda x, y: augment_image(x, y))
-    
-    train_dataset = train_dataset.concatenate(augmented_dataset).batch(batch_size = batch_size)
+    # augment된 데이터 추가
+    X_train_augmented = np.concatenate([X_train, np.array(augmented_images)], axis=0)
+    y_train_augmented = np.concatenate([y_train, np.array(y_train_non_happy)], axis=0)
 
-    # 검증 데이터에는 증강 미적용
-    val_dataset = val_dataset.map(lambda x, y: preprocess_image(x, y)).batch(batch_size = batch_size)
-
-    # 데이터 셔플 및 프리페치
-    train_dataset = train_dataset.shuffle(buffer_size=1000, seed=RANDOM_STATE).prefetch(tf.data.experimental.AUTOTUNE)
-    val_dataset = val_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-
-    return train_dataset, val_dataset
+    # Return the augmented train and validation data
+    return all_images, all_labels, X_train_augmented, y_train_augmented, X_val, y_val
--- train.py
+++ train.py
@@ -4,7 +4,9 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 import keras
-from sklearn.model_selection import train_test_split
+import sklearn
+from scikeras.wrappers import KerasClassifier
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.preprocessing import LabelEncoder
 from keras import layers, models
 import matplotlib.pyplot as plt
@@ -12,97 +14,91 @@ from sklearn.metrics import accuracy_score
 from data_loader import load_train_data
 from data_loader import NUM_CLASSES, DEFAULT_SIZE
 
-
-
-
 INPUT_SIZE = DEFAULT_SIZE #48
-BATCH_SIZE = 64
-EPOCHS = 30
+BATCH_SIZE = 32
+EPOCHS = 10
+
+#hyperparameter tuning
+#learning rate, optimizer, batch size
 
 # Data Load
-train_dataset, val_dataset = load_train_data(img_size=INPUT_SIZE, gray=True, normalization=True, flatten=False, batch_size=BATCH_SIZE)
+X, y, X_train, y_train, X_val, y_val = load_train_data(img_size=INPUT_SIZE, gray=True, normalization=True, flatten=False, batch_size=BATCH_SIZE)
 
 # 모델에 따라 추가적인 preprocessing 필요한 경우 있음 
-# def preprocess_with_densenet(image, label):
-#     image = tf.cast(image, tf.float32)  # 이미지 데이터를 float32로 변환
-#     image = keras.applications.densenet.preprocess_input(image)    
-#     return image, label
 
-# train_dataset = train_dataset.map(preprocess_with_densenet)
-# val_dataset = val_dataset.map(preprocess_with_densenet)
-
-# feature_extractor = tf.keras.applications.DenseNet169(input_shape=(INPUT_SIZE,INPUT_SIZE, 3),
-#                                                include_top=False,
-#                                                weights="imagenet")
-
-# 모델 정의
 model = models.Sequential([
-    # ex)
-    #RGB 사용할 경우 gray=False, input_shape=(INPUT_SIZE, INPUT_SIZE, 3))
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(INPUT_SIZE, INPUT_SIZE, 1)), 
-    layers.MaxPooling2D((2, 2)),
-    
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    
-    layers.Conv2D(128, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    
-    layers.Flatten(),
-    
-    layers.Dense(128, activation='relu'),
-    layers.Dropout(0.5),
-    
-    layers.Dense(NUM_CLASSES, activation='softmax')
-])
-
+        # relu 쓰면, kernel_initializer='he_normal'
+        # sigmoid, tanh 에는 default (glorot_normal)
+        layers.Input(shape=(INPUT_SIZE, INPUT_SIZE, 1)),  # Input 레이어 추가
+        layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dense(NUM_CLASSES, activation='softmax')
+    ])
 model.summary()
 
-# EarlyStopping, 사용하려면, callbacks에
+# 모델 정의
+def build_model(optimizer='adam', learning_rate=1e-3):
+    build_model = model    
+    if optimizer == 'adam':
+        optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    elif optimizer == 'rmsprop':
+        optimizer = keras.optimizers.RMSprop(learning_rate=learning_rate)
+    else:
+        optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
+    # 하이퍼파라미터 튜닝 설정
+    
+    build_model.compile(
+        optimizer=optimizer,
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy', keras.metrics.SparseTopKCategoricalAccuracy(k=2)]
+    )
+    
+    return build_model
+
+
+keras_clf = KerasClassifier(build_model, optimizer='adam', learning_rate=1e-3)
+
+param_distribs = {
+    "optimizer": ["adam", "rmsprop", "sgd"],
+    "learning_rate": [1e-6, 1e-5, 1e-4, 1e-3, 1e-2],
+}
+
+# EarlyStopping
 early_stopping = keras.callbacks.EarlyStopping(
     monitor='val_accuracy',
-    patience=3,
+    patience=5,
     restore_best_weights=True
 )
 
-# Model Chekpoint (가장 좋은 모델 저장)
+# Model Chekpoint (가장 좋은 모델 저장) 저장 안하는 게 좋을듯..? 사용하려면, callbacks에 추가
 model_checkpoint = keras.callbacks.ModelCheckpoint(
-    './models/CNN.keras',  # 모델 저장 경로, model 이름으로
+    './models/????.keras',  # 모델 저장 경로, model 이름으로
     monitor='val_accuracy',
     save_best_only=True,  # 가장 좋은 모델만 저장
     mode='max',  # val_accuracy가 최대일 때 저장
     verbose=1
 )
 
-# 모델 컴파일
-model.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy', keras.metrics.SparseTopKCategoricalAccuracy(k=2)]
-)
+
 
 # 모델 학습
-history = model.fit(
-    train_dataset,
+grid_search_cv = GridSearchCV(keras_clf, param_distribs, cv=StratifiedKFold(n_splits=5))
+grid_search_cv.fit(X, y, epochs=EPOCHS, callbacks=[early_stopping], verbose=1)
+
+best_model = grid_search_cv.best_estimator_
+
+history = best_model.fit(
+    X, y,  # X and y are your training data, no need to split again
     epochs=EPOCHS,
-    validation_data=val_dataset,
+    validation_data=(X_val, y_val),  # Use the validation data
     verbose=1,
-    callbacks=[model_checkpoint]
+    callbacks=[early_stopping]
 )
 
 # 모델 예측 및 정확도 계산
-y_true = []
-y_pred = []
-
-# train_dataset에서 예측
-for images, labels in val_dataset:
-    # 예측값 얻기
-    batch_pred = model.predict(images)  # 예측값 (배치 단위)
-    batch_pred = np.argmax(batch_pred, axis=1)  # 가장 높은 확률을 가진 클래스의 인덱스 추출
-    
-    # 실제 레이블 저장
-    y_true.extend(labels.numpy())  # 실제 레이블을 numpy 배열로 변환해서 저장
-    y_pred.extend(batch_pred)  # 예측값 저장
+y_true = y_val
+y_pred = np.argmax(best_model.predict(X_val), axis=1) 
 
 # accuracy_score 계산
 final_accuracy = accuracy_score(y_true, y_pred)
